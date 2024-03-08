6bb2547421214f4440eecb55580399ddfd09fcb8
dinogomez <dinogomez117@gmail.com>
2024-03-08 23:19:21 +0800

    chore: testbg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <div className="min-h-screen bg-neutral-950 ">
+        <body className={inter.className}>{children}</body>
+        <BackgroundBeams />
+      </div>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
+"use client";
+
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import { Button } from "@/components/ui/button";
+import GradientButton from "@/components/ui/gradient-button";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+    <div className="min-h-screen bg-neutral-950 w-full relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold italic">
           horizon
         </h1>
         <p></p>
-        <Button className="w-full">Login</Button>
+
+        <GradientButton className="w-full" text="Login" href="/login" />
       </div>
-      <BackgroundBeams />
     </div>
   );
 }
