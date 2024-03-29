38780b8217e92c4b26192a7ff8c3adf33c4ab62f
dinogomez <dinogomez117@gmail.com>
2024-03-30 03:26:11 +0800

    minor: toolbar qol

--- src/components/tiptap/toolbar.tsx
+++ src/components/tiptap/toolbar.tsx
@@ -10,9 +10,13 @@ import {
     Heading2,
     Minus,
     Underline,
+    HelpCircle,
+    Heading,
 } from "lucide-react";
 
 import { Toggle } from "../ui/toggle";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Label } from "../ui/label";
 
 type Props = {
     editor: Editor | null;
@@ -33,7 +37,7 @@ export function Toolbar({ editor }: Props) {
                         editor.chain().focus().toggleHeading({ level: 2 }).run()
                     }
                 >
-                    <Heading2 className="h-4 w-4" />
+                    <Heading className="h-4 w-4" />
                 </Toggle>
                 <Toggle
                     size="sm"
@@ -72,7 +76,7 @@ export function Toolbar({ editor }: Props) {
                     <Strikethrough className="h-4 w-4 " />
                 </Toggle>
             </div>
-            <div className="">
+            <div className="flex items-center">
                 <Toggle
                     size="sm"
                     pressed={editor.isActive("bulletList")}
@@ -100,6 +104,62 @@ export function Toolbar({ editor }: Props) {
                 >
                     <Minus className="h-4 w-4" />
                 </Toggle>
+
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <HelpCircle className="h-4 w-4 fill-primary text-white hover:cursor-pointer" />
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80">
+                        <div className="grid gap-4">
+                            <div className="space-y-2">
+                                <h4 className="font-medium leading-none">
+                                    Toolbar
+                                </h4>
+                                <p className="text-sm text-muted-foreground">
+                                    These are the toolbar items.
+                                </p>
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="grid grid-cols-6 items-center gap-2">
+                                    <Heading className="h-4 w-4" />
+                                    <Label>Heading</Label>
+                                </div>
+                                <div className="grid grid-cols-6 items-center gap-2">
+                                    <Bold className="h-4 w-4" />
+                                    <Label>Bold</Label>
+                                </div>
+                                <div className="grid grid-cols-6 items-center gap-2">
+                                    <Italic className="h-4 w-4" />
+                                    <Label>Italic</Label>
+                                </div>
+                                <div className="grid grid-cols-6 items-center gap-2">
+                                    <Underline className="h-4 w-4 " />
+                                    <Label>Underline</Label>
+                                </div>
+                                <div className="grid grid-cols-6 items-center gap-2">
+                                    <Strikethrough className="h-4 w-4 " />
+                                    <Label>Strikethrough</Label>
+                                </div>
+                                <div className="grid grid-cols-6 items-center gap-2">
+                                    <List className="h-4 w-4" />
+                                    <Label>List</Label>
+                                </div>
+                                <div className="grid grid-cols-6 items-center gap-2">
+                                    <ListOrdered className="h-4 w-4" />
+                                    <Label className="col-span-4">
+                                        Ordered List
+                                    </Label>
+                                </div>
+                                <div className="grid grid-cols-6 items-center gap-2">
+                                    <Minus className="h-4 w-4" />
+                                    <Label className="col-span-4">
+                                        Horizontal Ruler
+                                    </Label>
+                                </div>
+                            </div>
+                        </div>
+                    </PopoverContent>
+                </Popover>
             </div>
         </div>
     );
