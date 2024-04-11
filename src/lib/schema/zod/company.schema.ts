e52e8f8a1c011e7c06b5da93469883b8585f8272
dinogomez <dinogomez117@gmail.com>
2024-04-12 02:58:54 +0800

    refactor: schema changes

--- src/lib/schema/zod/company.schema.ts
+++ src/lib/schema/zod/company.schema.ts
@@ -4,31 +4,38 @@ const MAX_FILE_SIZE = 500000;
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
 export const CompanySchema = z.object({
-    companyName: z.string().min(1, "Company name is required"),
-    companyDesc: z.string().min(1, "Company description is required"),
-    companyEmail: z
+    company_Name: z.string().min(1, "Company name is required"),
+    company_Desc: z.string().min(1, "Company description is required"),
+    company_Email: z
         .string()
         .min(1, "Company Email is required")
         .email("Invalid email address"),
-    website: z
+    company_Website: z
         .string()
         .url("Please enter a valid https url.")
         .optional()
         .or(z.literal("")),
-    province: z.string().min(1, "Select a region first."),
-    city: z.string().min(1, "Select a province first."),
-    barangay: z.string().optional(),
-    industry: z
+    company_Province: z.string().min(1, "Select a province."),
+    company_City: z.string().min(1, "Select a province first."),
+    company_Barangay: z.string().optional(),
+    company_Industry: z
         .array(z.string())
         .refine((value) => value.some((item) => item), {
             message: "You have to select at least one industry.",
         }),
-    companyLogo: z
-        .custom<File>((val) => val instanceof File, "Company logo is required.")
-        .refine((file) => file.size <= MAX_FILE_SIZE, `Max file size is 5mb`)
-        .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
-            message: "Please choose a supported format.",
-        }),
-    numEmployee: z.string().min(1, "Number of employees is required"),
+    company_Logo: z.union([
+        z.string(),
+        z
+            .any()
+            .refine((val) => val instanceof File, "Company logo is required.")
+            .refine(
+                (file) => file.size <= MAX_FILE_SIZE,
+                `Max file size is 5mb`
+            )
+            .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
+                message: "Please choose a supported format.",
+            }),
+    ]),
+    company_NumEmployee: z.string().min(1, "Number of employees is required"),
     userId: z.string().optional(),
 });
